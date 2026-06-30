7cbee56053dea7213ae0fce2812597c0349d7073
przwth <przwth@tuta.io>
2026-06-30 20:17:08 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.go
@@ -0,0 +1,18 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	var freq [26]uint16
+	for i := 0; i < len(s); i++ {
+		freq[s[i]-'a']++
+		freq[t[i]-'a']--
+	}
+	for _, f := range freq {
+		if f != 0 {
+			return false
+		}
+	}
+
+	return true
+}
